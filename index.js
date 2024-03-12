fa11a7bab226a65f07e7d626f16ec71b20441bef
miimeee <alsalsl0819@gmail.com>
2024-03-12 14:22:20 +0900

    이미지 슬라이더 jQuery구현

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 $(function () {
-  var $prevButton = $(".arrow-btn:not(.active)");
-  var $nextButton = $(".arrow-btn.active");
+  const $prevButton = $(".arrow-btn:not(.active)");
+  const $nextButton = $(".arrow-btn.active");
 
-  var $sliderInner = $(".img-slider-inner");
+  const $sliderInner = $(".img-slider-inner");
+
+  const $firstPage = $(".pagenation-btn.active");
+  const $secondPage = $(".pagenation-btn:not(.active)");
 
   $prevButton.on("click", function () {
     $(this).removeClass("active");
     $nextButton.addClass("active");
+    $secondPage.removeClass("active");
+    $firstPage.addClass("active");
     $sliderInner.css("left", "0");
   });
 
   $nextButton.on("click", function () {
     $(this).removeClass("active");
+    $prevButton.addClass("active");
+    $firstPage.removeClass("active");
+    $secondPage.addClass("active");
+    $sliderInner.css("left", "-100%");
+  });
+
+  $firstPage.click(function () {
+    $secondPage.removeClass("active");
+    $(this).addClass("active");
+    $prevButton.removeClass("active");
+    $nextButton.addClass("active");
+    $sliderInner.css("left", "0");
+  });
+
+  $secondPage.click(function () {
+    $firstPage.removeClass("active");
+    $(this).addClass("active");
+    $nextButton.removeClass("active");
     $prevButton.addClass("active");
     $sliderInner.css("left", "-100%");
   });
